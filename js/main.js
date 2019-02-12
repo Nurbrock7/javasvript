ceec0d1ad9fe8cc37de4a535e39d6446b0565493
nurbrock7 <nurbrock7@gmail.com>
2019-02-12 14:51:37 +0200

    update javasvript

---
+++ js/main.js
@@ -0,0 +1,19 @@
+console.log ("Hello JavaScript");
+let name = " Nur" + " Brock" ;
+    age = "23" ;
+    gender = "male" ;
+    cake = " red valvet baked cheesecake"
+    layers = " 2"
+    prices = " R50"
+
+
+console.log (name);
+console.log (age) ;
+console.log (gender) ;
+console.log (cake + layers + prices) ;
+
+let message = "Good Morning" ; 
+alert (message + name);
+
+console.log ("Good Morning. I have baked the valentines cake a " + layers + " layer," + cake, " at" + prices + " per piece")
+// prompt (message)
---
+++ js/task_1.js
@@ -0,0 +1,4 @@
+let message = "Good day"
+    place   = " Sevruga "
+    dish = "a Sushi train "
+    dessert = "" 
