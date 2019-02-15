44e0c4385c0fecb851988fd1931d5d6ce5ea074f
Warnita Adams <warnita.adams.rsa@gmail.com>
2019-02-15 11:12:46 +0200

    update javasvript

---
+++ js/conditions.js
@@ -0,0 +1,42 @@
+// let weather = prompt( "whats the weather like today");
+// if (weather === "Sunny") { console.log (weather + " Wear flip flops , shorts and a Vest. Dont forget your hat") ;
+//  }
+//   else if (weather === "Windy") { console.log ( weather + " wear a jeans, long sleeve top with a pair of closed top shoes");
+// }
+
+// else if  (weather === "Raining" ) console.log ( weather + " wear a rain coat with a thick pants and boots. dont forget your umbrella!") ;  
+
+// else console.log ("Not Valid")
+// "AND" operator
+
+// 
+
+// if (number >= 0 && number <= 100) { console.log (number + " is between 0 and 100, both included");
+// }
+
+// the && operator  (AND) can apply to both types of Boolean vaues. true will only be the result of the statement if both conditions are met.
+
+// task - write a program that will help people what to wear based on the weather. use the switch and break statement
+let clean = prompt ("Who is going to clean the class?")
+    Nur = " Nur will clean on a Monday"
+    Imraan = " Imo will clean on a Tuesday"
+    Liam = " Liam will clean on a Wednesday"
+
+
+switch (clean) {
+    case "Nur": console.log (Nur)
+        
+        break;
+
+    case "Imraan" : console.log (Imraan)
+        
+        break;
+        
+
+    case "Liam" : console.log (Liam)
+
+        break;
+
+    default:
+        break;
+}
